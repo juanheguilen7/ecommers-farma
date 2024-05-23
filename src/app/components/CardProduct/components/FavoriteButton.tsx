81c508e0f3e7f35fd7fca431541a45bd5ea118fd
Juan <juan_heguilen@hotmail.com>
2024-05-23 10:37:59 -0300

    btnfavorite

--- src/app/components/CardProduct/components/FavoriteButton.tsx
+++ src/app/components/CardProduct/components/FavoriteButton.tsx
@@ -1,17 +1,22 @@
-// components/Buttons/FavoriteButton.tsx
 import React, { useState } from 'react';
 import Image from 'next/image';
 
 interface FavoriteButtonProps {
     id: string;
     isFavorite: boolean;
-    userBookmarkId: string;
+    userBookmarkId?: string; // Hacemos que userBookmarkId sea opcional
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ id, isFavorite, userBookmarkId }) => {
     const [favorite, setFavorite] = useState(isFavorite);
 
     const handlePushFav = async (id: string) => {
+        if (!userBookmarkId) {
+            // Si no hay userBookmarkId, salimos de la función
+            console.warn('userBookmarkId is not defined. Cannot change favorite status.');
+            return;
+        }
+
         const newFavoriteStatus = !favorite;
         setFavorite(newFavoriteStatus);
         const sendData = {
@@ -26,7 +31,7 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ id, isFavorite, userBoo
     };
 
     return (
-        <button onClick={() => handlePushFav(id)}>
+        <button onClick={() => handlePushFav(id)} disabled={!userBookmarkId}>
             <Image src={favorite ? '/icons/heart-filled.svg' : '/icons/heart.svg'} alt='iconHeart' width={20} height={20} />
         </button>
     );
